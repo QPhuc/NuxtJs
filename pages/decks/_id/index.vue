46a7e70e3384e9b6d8b57273ef1610a55200b861
Qphuc <tqp1610@gmail.com>
2021-08-14 21:48:27 +0700

    validate

--- pages/decks/_id/index.vue
+++ pages/decks/_id/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>id</div>
+  <div>
+    <h1>Your decks {{ $route.params.id }} is active</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^[0-9]/.test(params.id);
+  }
+}
+</script>
