edf3f7d4ef31ed4e8bbf0bbd0ab9071d62bb84f3
Qphuc <tqp1610@gmail.com>
2021-08-15 11:24:03 +0700

    css deck page

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    './assets/rhpteam.min.css',
     './assets/styles.global.css'
   ],
 
--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -1,8 +1,46 @@
 <template>
   <div>
-    <h1>Your Decks, please enter number of decks show:</h1>
-    <input type="text" v-model="deckID" placeholder="Please enter deckId" />
-    <button @click="showDeck">Go to Deck</button>
+    <div class="ct">
+      <h1>List of your decks:</h1>
+      <ul class="decks-list">
+        <li>
+          <div class="card deck-card">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsK6sNyy6Ds6Q-nFnOCBVoK_IaCLJEXeyI6w&usqp=CAU"
+              alt="Thumbnail card"
+            />
+            <div class="card_body">
+              <h3>Title card</h3>
+              <p>Description card</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="card deck-card">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsK6sNyy6Ds6Q-nFnOCBVoK_IaCLJEXeyI6w&usqp=CAU"
+              alt="Thumbnail card"
+            />
+            <div class="card_body">
+              <h3>Title card</h3>
+              <p>Description card</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="card deck-card">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsK6sNyy6Ds6Q-nFnOCBVoK_IaCLJEXeyI6w&usqp=CAU"
+              alt="Thumbnail card"
+            />
+            <div class="card_body">
+              <h3>Title card</h3>
+              <p>Description card</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -23,4 +61,28 @@ export default {
 </script>
 
 <style scoped>
+.decks-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.decks-list li {
+  margin-bottom: 1rem;
+}
+
+.decks-list li:last-child {
+  margin-bottom: 0;
+}
+
+.deck-card {
+  display: flex;
+  flex-direction: row;
+  height: 160px;
+}
+
+.deck-card img {
+  width: 160px;
+  height: auto;
+}
 </style>
